b437929d183ff368e6b3b1b23d3750f1f0e41289
Paul Muguro <pauldeamosson@gmail.com>
2022-07-05 18:26:27 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,9 @@
-#include "_putchar"
+#include<unistd.h
+#include "main.h"
 /**
- * main - Entry point
- * Return: Always 0
- */
-int main(void)
+ * *comments
+ * */
+Int _putchar(char c)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+return(write (1, & c, 1));
 }
